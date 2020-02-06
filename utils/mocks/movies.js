857037a6bf434a51bbc021a840874344c7d44354
Emiliano Rago <emiliano_rago@intranet.itresources>
2020-02-06 17:34:53 -0300

    curso platzi movies NodeJs

---
+++ utils/mocks/movies.js
@@ -0,0 +1,153 @@
+const moviesMock = [{
+    "id": "e1e9e129-710d-45e2-81a2-d13d7f89f5ec",
+    "title": "Return of the Prodigal Son, The (Návrat ztraceného syna)",
+    "year": 1994,
+    "cover": "http://dummyimage.com/200x212.jpg/5fa2dd/ffffff",
+    "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus. Praesent lectus.\n\nVestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.",
+    "duration": 1910,
+    "contentRating": "NC-17",
+    "source": "https://marriott.com/fermentum/justo/nec/condimentum/neque/sapien.json",
+    "tags": [
+      "Drama",
+      "Action|Crime|Drama|Thriller",
+      "Comedy|Drama|Romance",
+      "Comedy"
+    ]
+  }, {
+    "id": "49ee7206-5974-43ed-ae44-e97140bee5b7",
+    "title": "Little Stiff, A",
+    "year": 2010,
+    "cover": "http://dummyimage.com/200x125.jpg/5fa2dd/ffffff",
+    "description": "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.",
+    "duration": 2011,
+    "contentRating": "G",
+    "source": "http://nymag.com/odio.xml",
+    "tags": [
+      "Documentary",
+      "Drama|Mystery|Sci-Fi|Thriller",
+      "Documentary",
+      "Documentary",
+      "Drama"
+    ]
+  }, {
+    "id": "2982b33c-e646-48fb-9d07-db6e1d0189fd",
+    "title": "Meu Passado Me Condena: O Filme",
+    "year": 2005,
+    "cover": "http://dummyimage.com/151x111.png/cc0000/ffffff",
+    "description": "Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.\n\nCurabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.\n\nInteger tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.",
+    "duration": 2050,
+    "contentRating": "PG",
+    "source": "https://java.com/primis/in/faucibus/orci/luctus/et.html",
+    "tags": [
+      "Adventure|Children",
+      "Comedy|Fantasy|Horror",
+      "Drama",
+      "Drama"
+    ]
+  }, {
+    "id": "cd6da78d-5f31-41e5-8927-a68dad32d7be",
+    "title": "Seduced and Abandoned (Sedotta e abbandonata)",
+    "year": 1994,
+    "cover": "http://dummyimage.com/249x221.jpg/dddddd/000000",
+    "description": "Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.\n\nIn congue. Etiam justo. Etiam pretium iaculis justo.\n\nIn hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.",
+    "duration": 2058,
+    "contentRating": "PG-13",
+    "source": "http://51.la/felis.aspx",
+    "tags": [
+      "Drama|Romance",
+      "Action|Adventure|Drama|Thriller|IMAX",
+      "(no genres listed)",
+      "Drama"
+    ]
+  }, {
+    "id": "c2f1a60e-8bbd-4526-b25c-7bc092a23922",
+    "title": "Good Luck Chuck",
+    "year": 2002,
+    "cover": "http://dummyimage.com/114x144.jpg/dddddd/000000",
+    "description": "Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.",
+    "duration": 1908,
+    "contentRating": "PG-13",
+    "source": "http://creativecommons.org/ac/enim.json",
+    "tags": [
+      "Documentary",
+      "Drama|Mystery",
+      "Documentary",
+      "Action|Animation|Fantasy",
+      "Documentary"
+    ]
+  }, {
+    "id": "798a8221-65ff-42fe-80a2-3291094723f4",
+    "title": "Four Men and a Prayer",
+    "year": 1993,
+    "cover": "http://dummyimage.com/108x245.png/cc0000/ffffff",
+    "description": "Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.\n\nAenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.\n\nCurabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.",
+    "duration": 1926,
+    "contentRating": "G",
+    "source": "http://liveinternet.ru/amet/nunc.jpg",
+    "tags": [
+      "Drama",
+      "Drama",
+      "Comedy"
+    ]
+  }, {
+    "id": "c28087ef-a777-4bab-8948-6c37d522d4a9",
+    "title": "Examined Life",
+    "year": 2009,
+    "cover": "http://dummyimage.com/156x250.bmp/5fa2dd/ffffff",
+    "description": "Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.\n\nCum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.\n\nEtiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.",
+    "duration": 1968,
+    "contentRating": "PG-13",
+    "source": "http://accuweather.com/condimentum/id.json",
+    "tags": [
+      "Comedy|Documentary",
+      "Action|Adventure|Crime|Thriller"
+    ]
+  }, {
+    "id": "9ebff267-6ae7-44d2-ad49-1255d7a62c7c",
+    "title": "Once Upon a Time in China II (Wong Fei-hung Ji Yi: Naam yi dong ji keung)",
+    "year": 1996,
+    "cover": "http://dummyimage.com/191x114.jpg/5fa2dd/ffffff",
+    "description": "Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.\n\nIn quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.",
+    "duration": 1972,
+    "contentRating": "G",
+    "source": "http://usnews.com/tincidunt/nulla/mollis.jpg",
+    "tags": [
+      "Adventure|Comedy|Fantasy|Musical",
+      "Action|Animation|Sci-Fi|Thriller",
+      "Comedy|Drama",
+      "(no genres listed)"
+    ]
+  }, {
+    "id": "256c0eb1-0e97-4194-9ef9-c9f9ce0b0f86",
+    "title": "Save the Date",
+    "year": 2011,
+    "cover": "http://dummyimage.com/116x235.bmp/5fa2dd/ffffff",
+    "description": "Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.\n\nInteger tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.\n\nPraesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.",
+    "duration": 1979,
+    "contentRating": "PG-13",
+    "source": "https://paginegialle.it/in/hac/habitasse/platea/dictumst/maecenas.png",
+    "tags": [
+      "Drama|Romance",
+      "Comedy|Documentary"
+    ]
+  }, {
+    "id": "b16777df-e21d-408f-b964-8b214dce6958",
+    "title": "Man from Monterey, The",
+    "year": 2001,
+    "cover": "http://dummyimage.com/221x112.jpg/cc0000/ffffff",
+    "description": "Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.\n\nNam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.\n\nCurabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.",
+    "duration": 2013,
+    "contentRating": "NC-17",
+    "source": "https://gov.uk/nisl.html",
+    "tags": [
+      "Drama",
+      "Drama",
+      "Drama|Thriller",
+      "Animation|Drama|Fantasy",
+      "Action|Animation|Comedy"
+    ]
+  }];
+
+  module.exports = {
+      moviesMock
+  }
